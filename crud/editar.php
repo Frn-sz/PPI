e5021c10cdd5785cb33ff3c83e17029865b31dfd
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2022-06-14 15:15:21 -0300

    edição do form

--- crud/editar.php
+++ crud/editar.php
@@ -8,19 +8,6 @@
 
     $infos = mysqli_fetch_assoc($result);
 
-    function puxarinfos($i){
-        include "conexao.php";
-        $id = $_GET['id'];
-        $sql = "SELECT * FROM `biografias` WHERE id=$id";
-    
-        $result = mysqli_query($conexao, $sql);
-    
-        $infos = mysqli_fetch_assoc($result);
-
-        echo $infos[$i];
-    
-    }
-
 ?>
 
 <!DOCTYPE html>
@@ -44,32 +31,32 @@
     <main class= "container">
         <h1> Edição de obras:</h1>
 <form action = "edita.php" method = "post" enctype="multipart/form-data">
-        <p>Apelido: <input type = "text" name="apelido" value = <?= puxarinfos('apelido')?> ></p>
-        <p>Nome verdadeiro: <input type = "text" name="nome"  value = <?= puxarinfos('nome')?>></p>
+        <p>Apelido: <input type = "text" name="apelido" value = <?= $info['apelido']?> ></p>
+        <p>Nome verdadeiro: <input type = "text" name="nome"  value = <?= $info['nome']?>></p>
         <p>Área de conhecimento: <select name = "areac">
             <option value = "Física">Física</option>
             <option value = "Sociologia">Sociologia</option>
             <option value = "História">História</option>
         </select></p>
-        <input type="hidden" name = "id" value = <?= puxarinfos('id');?>>
-        <p>Data de Nascimento: <input type = "date" name="dataNas" value = <?= puxarinfos('dataNas')?>></p>
-        <p>Data de Falecimento: <input type = "date" name="dataFal" value = <?= puxarinfos('dataFal')?>></p>
-        <p>Local de Nascimento: <input type = "text" name="localNas" value = <?= puxarinfos('localNas')?>></p>
-        <p>Local de Nascimento: <input type = "text" name="localFal" value = <?= puxarinfos('localFal')?>></p>
-        <p>Pais: <input type = "text" name="pais" value =  <?= puxarinfos('pais')?>></p>
-        <p>Pessoas famosas na família: <input type = "text" name="pesFamosa" value = <?= puxarinfos('pesFamosa')?>></p>
-        <p>Grupo Social: <input type = "text" name="gpSocial"value =  <?= puxarinfos('gpSocial')?>></p>
-        <p>Formação: <input type = "text" name="formacao" value = <?= puxarinfos('formacao')?>></p>
-        <p>Profissão: <input type = "text" name="profissao"value =  <?= puxarinfos('profissao')?>></p>
-        <p>Mudança de grupo social: <input type = "text" name="mudancaGp"value =  <?= puxarinfos('mudancaGp')?>></p>
-        <p>Evento Histórico:  <input type = "text" name="eventoHist"value =  <?= puxarinfos('eventoHist')?>></p>
-        <p>Raça e Gênero <input type = "text" name="racaGen" value = <?= puxarinfos('racaGen')?>></p>
-        <p>Contribuição: <input type = "text" name="contribuicao" value = <?= puxarinfos('contribuicao')?>></p>
-        <p>Legado Permanente: <input type = "text" name="legadoPerm"value =  <?= puxarinfos('legadoPerm')?>></p>
-        <p>Ato Político: <input type = "text" name="atoPoli" value = <?= puxarinfos('atoPoli')?>></p>
-        <p>Ideias: <input type = "text" name="ideias" value = <?= puxarinfos('ideias')?>></p>
-        <p>Frases marcantes: <input type = "text" name="frase" value = <?= puxarinfos('frase')?>></p>
-        <p>Referências: <input type = "text" name="referencias" value = <?= puxarinfos('referencias')?>></p>
+        <input type="hidden" name = "id" value = <?= $info['id']; ?>>
+        <p>Data de Nascimento: <input type = "date" name="dataNas" value = <?= $info['dataNas']?>></p>
+        <p>Data de Falecimento: <input type = "date" name="dataFal" value = <?= $info['dataFal'] ?>></p>
+        <p>Local de Nascimento: <input type = "text" name="localNas" value = <?= $info['localNas']?>></p>
+        <p>Local de Nascimento: <input type = "text" name="localFal" value = <?= $info['localFal']?>></p>
+        <p>Pais: <input type = "text" name="pais" value =  <?= $info['pais']?>></p>
+        <p>Pessoas famosas na família: <input type = "text" name="pesFamosa" value = <?= $info['pesFamosa']?>></p>
+        <p>Grupo Social: <input type = "text" name="gpSocial"value =  <?= $info['gpSocial']?>></p>
+        <p>Formação: <input type = "text" name="formacao" value = <?= $info['formacao']?>></p>
+        <p>Profissão: <input type = "text" name="profissao"value =  <?= $info['profissao']?>></p>
+        <p>Mudança de grupo social: <input type = "text" name="mudancaGp"value =  <?= $info['mudancaGp']?>></p>
+        <p>Evento Histórico:  <input type = "text" name="eventoHist"value =  <?= $info['eventoHist']?>></p>
+        <p>Raça e Gênero: <textarea class = "materialize-textarea"  name="racaGen" value = <?= $info['racaGen'] ?> ></textarea></p>
+        <p>Contribuição: <textarea class = "materialize-textarea"  name="contribuicao" value = <?= $info['racaGen']?>></textarea></p>
+        <p>Legado Permanente: <textarea class = "materialize-textarea"  name="legadoPerm" value = <?= $info['legadoPerm']?>></textarea></p>
+        <p>Ato Político: <textarea class = "materialize-textarea"  name="atoPoli" value = <?= $info['atoPoli']?>></textarea></p>
+        <p>Ideias: <textarea class = "materialize-textarea"  name="ideias" value = <?= $info['racaGen']?>></textarea></p>
+        <p>Frases marcantes: <input type = "text" name="frase" value = <?= $info['frase'] ?>></p>
+        <p>Referências: <textarea class = "materialize-textarea"  name="referencias" value = <?= $info['referencias']?>></textarea></p>
         <p>Imagem: <input type = "file" name = "arquivo"> </p>
         <p><input type = "reset" value="Desfazer mudanças"></p>
         <p><input type = "submit" value = "Enviar"></p>
