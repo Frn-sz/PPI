27fa7768f165f40df5928073759e068c39a33370
Fernando Medeiros <cilteixeira2011@gmail.com>
2022-06-21 14:58:37 -0300

    a
    arrumei o editar

--- crud/editar.php
+++ crud/editar.php
@@ -10,41 +10,197 @@
 
     <?php require_once "../cabecalho.php" ?>
   
+    <style type="text/css">
+
+/* label focus color */
+.input-field input[type=text]:focus + label {
+  color: #4e342e !important;
+}
+/* label underline focus color */
+.input-field input[type=text]:focus {
+  border-bottom: 1px solid #4e342e !important;
+  box-shadow: 0 1px 0 0 #4e342e !important;
+}
+/* label focus color */
+.input-field input[type=date]:focus + label {
+  color: #4e342e !important;
+}
+/* label underline focus color */
+.input-field input[type=date]:focus {
+  border-bottom: 1px solid #4e342e !important;
+  box-shadow: 0 1px 0 0 #4e342e !important;
+}
+/* label focus color */
+.input-field textarea:focus + label {
+  color: #4e342e !important;
+}
+/* label underline focus color */
+.input-field textarea:focus {
+  border-bottom: 1px solid #4e342e !important;
+  box-shadow: 0 1px 0 0 #4e342e !important;
+}
+
+</style>
+
 <body>
     <main class= "container">
         <h1> Edição de obras:</h1>
-        <form action = "edita.php" method = "post" enctype="multipart/form-data">
-        <p>Apelido: <input type = "text" name="apelido" value = <?= $info['apelido']?>></p>
-        <p>Nome verdadeiro: <input type = "text" name="nome" value = <?= $info['nome']?>></p>
-        <p>Área de conhecimento: 
-            
-        <select name = "areac">
 
+        <form action = "edita.php" method = "post" enctype="multipart/form-data">
+
+        <div class="row">
+                <div class="input-field col s12">
+                    <input id="apelido" type="text" name="apelido" value = <?= $info['apelido']?>>
+                    <label for="apelido">Apelido</label>
+                </div>
+            </div>
+        
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="nome" type="text" name="nome" value = <?= $info['nome']?>>
+                    <label for="nome">Nome verdadeiro</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+            <select id = "select" name = "areac">
+            
             <option value = "Física">Física</option>
             <option value = "Sociologia">Sociologia</option>
             <option value = "História">História</option>
 
-        </select></p>
+            </select>
+            <label for = "select">Área de conhecimento</label>
+            </div>
+            </div>
        
-      
-        <input id= "dataNasc" type = "date" name="dataNasc" value = <?= $info['dataNas']?>></p>
-        <input id= "dataFal" type = "date" name="dataFal" value = <?= $info['dataFal']?>></p>
-        <p>Local de nascimento: <input type = "text" name="localNas" value = <?= $info['localNas']?>></p>
-        <p>Local de nascimento: <input type = "text" name="localFal" value = <?= $info['localFal']?>></p>
-        <p>Pais: <input type = "text" name="pais" value = <?= $info['pais']?>></p>
-        <p>Grupo social: <input type = "text" name="gpSocial" value = <?= $info['gpSocial']?>></p>
-        <p>Pessoas famosas na família: <input type = "text" name="pesFamosa" value = <?= $info['pesFamosa']?>></p>
-        <p>Formação: <input type = "text" name="formacao" value = <?= $info['formacao']?>></p>
-        <p>Profissão: <input type = "text" name="profissao" value = <?= $info['profissao']?>></p>
-        <p>Mudança de grupo social: <input type = "text" name="mudancaGp" value = <?= $info['mudancaGp']?>></p>
-        <p>Evento histórico:  <input type = "text" name="eventoHist" value = <?= $info['eventoHist']?>></p>
-        <p>Raça e gênero: <textarea class = "materialize-textarea" name="racaGen" ><?= $info['racaGen']?></textarea></p>
-        <p>Contribuições: <textarea class = "materialize-textarea"  name="contribuicao"  ><?= $info['contribuicao']?></textarea></p>
-        <p>Legado permanente: <textarea class = "materialize-textarea"  name="legadoPerm"><?= $info['legadoPerm']?></textarea></p>
-        <p>Ato político: <textarea  class = "materialize-textarea" name="atoPoli"  ><?= $info['atoPoli']?></textarea></p>
-        <p>Ideias: <textarea  class = "materialize-textarea"  name="ideias"><?= $info['ideias']?></textarea></p>
-        <p>Frases marcantes: <input type = "text" name="frase" value = <?= $info['apelido']?>></p>
-        <p>Referências: <textarea class = "materialize-textarea"  name="referencias"> <?= $info['referencias']?></textarea></p>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id= "dataNasc" placeholder="" type = "date" name="dataNasc" value = <?= $info['dataNas']?>></p>
+                    <label for="dataNasc">Data de nascimento</label>
+                </div>
+            </div>
+
+            <div class="row">
+                 <div class="input-field col s12">
+                    <input id= "dataFal" placeholder="" type = "date" name="dataFal" value = <?= $info['dataFal']?>></p>
+                    <label for="dataFal">Data de falecimento</label>
+                </div>
+            </div>
+       
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="localNas" type="text" name="localNas" value = <?= $info['localNas']?>>
+                    <label for="localNas">Local de nascimento</label>
+                </div>
+            </div>
+        
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="localFal" type="text" name="localFal" value = <?= $info['localFal']?>>
+                    <label for="localFal">Local de falecimento</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="pais" type="text" name="pais" value = <?= $info['pais']?>>
+                    <label for="pais">Pais</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="gpSocial" type="text" name="gpSocial" value = <?= $info['gpSocial']?>>
+                    <label for="gpSocial">Grupo Social</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="pesFamosa" type="text" name="pesFamosa" value = <?= $info['pesFamosa']?>>
+                    <label for="pesFamosa">Pessoas famosas na família</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="formacao" type="text" name="formacao" value = <?= $info['formacao']?>>
+                    <label for="formacao">Formação</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="profissao" type="text" name="profissao" value = <?= $info['profissao']?>>
+                    <label for="profissao">Profissão</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="mudancagp" type="text" name="mudancagp" value = <?= $info['mudancaGp']?>>
+                    <label for="mudancagp">Mudança de grupo social</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="eventoHist" type="text" name="eventoHist" value = <?= $info['eventoHist']?>>
+                    <label for="eventoHist">Evento histórico</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea id="racagen" class = "materialize-textarea" name="racagen"><?= $info['racaGen']?></textarea>
+                    <label for="racagen">Raça e gênero</label>
+                </div>
+            </div>
+        
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="contribuicao" type="text" name="contribuicao"><?= $info['contribuicao']?></textarea>
+                    <label for="contribuicao">Contribuições</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="legadoPerm" type="text" name="legadoPerm"><?= $info['legadoPerm']?></textarea>
+                    <label for="legadoPerm">Legado permanente</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="atoPoli" type="text" name="atoPoli"><?= $info['atoPoli']?></textarea>
+                    <label for="atoPoli">Ato político</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="ideias" type="text" name="ideias"><?= $info['ideias']?></textarea>
+                    <label for="ideias">Ideias</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="frase" type="text" name="frase" value = <?= $info['apelido']?>>
+                    <label for="frase">Frases marcantes</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="referencias" type="text" name="referencias"><?= $info['referencias']?></textarea>
+                    <label for="referencias">Referências</label>
+                </div>
+            </div>
+
         <div class="file-field input-field">
       <div class="btn brown darken-4">
         <span><i class = "material-icons">add_a_photo</i></span>
