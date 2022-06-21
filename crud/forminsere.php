427e24557a44eb4e128f8d7d1ea68bc61a85f20f
Fernando Medeiros <cilteixeira2011@gmail.com>
2022-06-21 13:49:36 -0300

    a
    arrumei o forminsere

--- crud/forminsere.php
+++ crud/forminsere.php
@@ -7,60 +7,171 @@
 <body>
 
     <main class= "container">
-    
+    <h3 class="center-align">Cadastro de biografia:</h3>
 
        
         <form action = "insere.php" method = "post" enctype="multipart/form-data">
-        <p>Apelido: <input type = "text" name="apelido"></p>
-        <p>Nome verdadeiro: <input type = "text" name="nome"></p>
-        <p>Área de conhecimento: 
-            
-        <select name = "areac">
+
+        <div class="row">
+                <div class="input-field col s12">
+                    <input id="apelido" type="text" name="apelido">
+                    <label for="apelido">Apelido</label>
+                </div>
+            </div>
+
+        <div class="row">
+                <div class="input-field col s12">
+                    <input id="nome" type="text" name="nome">
+                    <label for="nome">Nome verdadeiro</label>
+                </div>
+            </div>
+        
+        <div class="input-field col s12">    
+            <select name = "areac">
 
             <option value = "Física">Física</option>
             <option value = "Sociologia">Sociologia</option>
             <option value = "História">História</option>
 
-        </select></p>
+            </select>
+            <label>Área de conhecimento</label>
+         </div>
        
-      
-        <input id= "dataNasc" type = "date" name="dataNasc"></p>
-        <input id= "dataFal" type = "date" name="dataFal"></p>
-        <p>Local de nascimento: <input type = "text" name="localNas"></p>
-        <p>Local de nascimento: <input type = "text" name="localFal"></p>
-        <p>Pais: <input type = "text" name="pais"></p>
-        <p>Grupo social: <input type = "text" name="gpSocial"></p>
-        <p>Pessoas famosas na família: <input type = "text" name="pesFamosa"></p>
-        <p>Formação: <input type = "text" name="formacao"></p>
-        <p>Profissão: <input type = "text" name="profissao"></p>
-        <p>Mudança de grupo social: <input type = "text" name="mudancagp"></p>
-        <p>Evento histórico:  <input type = "text" name="eventoHist"></p>
-        <p>Raça e gênero: <textarea class = "materialize-textarea" name="racagen"></textarea></p>
-        <p>Contribuições: <textarea class = "materialize-textarea"  name="contribuicao"></textarea></p>
-        <p>Legado permanente: <textarea class = "materialize-textarea"  name="legadoPerm"></textarea></p>
-        <p>Ato político: <textarea  class = "materialize-textarea" name="atoPoli"></textarea></p>
-        <p>Ideias: <textarea  class = "materialize-textarea"  name="ideias"></textarea></p>
-        <p>Frases marcantes: <input type = "text" name="frase"></p>
-        <p>Referências: <textarea class = "materialize-textarea"  name="referencias"></textarea></p>
-        <div class="file-field input-field">
-      <div class="btn brown darken-4">
-        <span><i class = "material-icons">add_a_photo</i></span>
-        <input type="file">
-      </div>
-      <div class="file-path-wrapper">
-        <input class="file-path validate" type="text" placeholder="Adicione uma foto">
-      </div>
-    </div>
-    <div class="row">
-        <div class="col offset-s6">
-    <a class="btn-floating  waves-effect waves-light brown darken-4"><i class="material-icons">restart_alt</i></a>
-      
-        <button class="btn-floating waves-effect waves-light brown darken-4" type="submit" name="action">
-        <i class="material-icons right">check</i>
-        </button>
-        </div>
-        </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id= "dataNasc" placeholder="" type = "date" name="dataNasc"></p>
+                    <label for="dataNasc">Data de nascimento</label>
+                </div>
+            </div>
+             <div class="row">
+                 <div class="input-field col s12">
+                    <input id= "dataFal" placeholder="" type = "date" name="dataFal"></p>
+                    <label for="dataFal">Data de falecimento</label>
+                </div>
+            </div>
+       
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="localNas" type="text" name="localNas">
+                    <label for="localNas">Local de nascimento</label>
+                </div>
+            </div>
+        
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="localFal" type="text" name="localFal">
+                    <label for="localFal">Local de falecimento</label>
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="pais" type="text" name="pais">
+                    <label for="pais">Pais</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="gpSocial" type="text" name="gpSocial">
+                    <label for="gpSocial">Grupo Social</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="pesFamosa" type="text" name="pesFamosa">
+                    <label for="pesFamosa">Pessoas famosas na família</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="formacao" type="text" name="formacao">
+                    <label for="formacao">Formação</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="profissao" type="text" name="profissao">
+                    <label for="profissao">Profissão</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="mudancagp" type="text" name="mudancagp">
+                    <label for="mudancagp">Mudança de grupo social</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="eventoHist" type="text" name="eventoHist">
+                    <label for="eventoHist">Evento histórico</label>
+                </div>
+            </div>
+       
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea id="racagen" class = "materialize-textarea" name="racagen"></textarea>
+                    <label for="racagen">Raça e gênero</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="contribuicao" type="text" name="contribuicao"></textarea>
+                    <label for="contribuicao">Contribuições</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="legadoPerm" type="text" name="legadoPerm"></textarea>
+                    <label for="legadoPerm">Legado permanente</label>
+                </div>
+            </div>
+    
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="atoPoli" type="text" name="atoPoli"></textarea>
+                    <label for="atoPoli">Ato político</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="ideias" type="text" name="ideias"></textarea>
+                    <label for="ideias">Ideias</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="frase" type="text" name="frase">
+                    <label for="frase">Frases marcantes</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                    <textarea class="materialize-textarea" id="referencias" type="text" name="referencias"></textarea>
+                    <label for="referencias">Referências</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s12">
+                  <p>Imagem: <input id="arquivo" type="file" name="arquivo"></p>
+                </div>
+            </div>
+        
+        
+        
+        <p><input type = "reset" value="Redefinir"></p>
+        <p><input type = "submit" value = "Enviar"></p>
     </form>
 
 </main>
