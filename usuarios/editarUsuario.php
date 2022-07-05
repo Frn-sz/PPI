acc1c0dceb6ecff473dfeab46e0a56ca917f5b26
Fernando Medeiros <fernandogdemedeirosw@gmail.com>
2022-07-05 14:35:49 -0300

    textareainsere

--- usuarios/editarUsuario.php
+++ usuarios/editarUsuario.php
@@ -5,5 +5,13 @@ if(!isset($_SESSION)){
 if(!isset($_SESSION['id_usuario'])){
     header("location:../crud");
 }
+include('../conexao.php');
 $id_usuario = $_SESSION['id_usuario'];
+$email = $_POST['email'];
+$nome = $_POST['nome'];
+$existe = false;
+$sqlemail = "SELECT email FROM usuario WHERE id!='$id_usuario'";
+$resultSet = mysqli_query($conexao, $sqlemail);
+$verificacao
+$sql = "UPDATE usuarios SET email='$email', nome='$nome' WHERE id_usuario='$id_usuario'";
 ?>
